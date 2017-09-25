7341cfb5aa2b5f3ea2de78d8919c92ef3e21396c
alterkait <alterkait2001@icloud.com>
2017-09-25 09:50:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
   #include <iostream>
   #include <cstdlib>
+  #include <fstream>
   #include <algorithm>
   
   using namespace std;
   int main()
-  
+  {
+    string name;
+    cout << "Hello, what is your name?\n" << endl;
+    cin >> name;
+    
+    int BirthYear;
+    cout << "Hi, " << name << " when were you born?" << endl;
+    cin >> BirthYear;
+    
+    int ThisYear, Age;
+    ThisYear = 2017;
+    Age = ThisYear - BirthYear;
+    cout << "So you are " << Age << " years old.." << endl;
+      
+    
+    
+    return 0;
+  }
